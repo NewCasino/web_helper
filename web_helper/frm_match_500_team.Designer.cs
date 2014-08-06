3dfbadae736b094697164e4b1eb7228490d50ed4
yanglong <598251812@qq.com>
2014-08-06 23:06:20 +0800

    get team name info from 500 website
    mofify frm_match_500_team
    get team name info from 500 website

--- web_helper/frm_match_500_team.Designer.cs
+++ web_helper/frm_match_500_team.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_step_3 = new System.Windows.Forms.Button();
+            this.btn_step_2 = new System.Windows.Forms.Button();
             this.btn_step_1 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.Text = new System.Windows.Forms.TabPage();
             this.txt_result = new System.Windows.Forms.TextBox();
-            this.btn_step_2 = new System.Windows.Forms.Button();
-            this.btn_step_3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.Text.SuspendLayout();
@@ -42,21 +42,43 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.btn_step_3);
             this.groupBox1.Controls.Add(this.btn_step_2);
             this.groupBox1.Controls.Add(this.btn_step_1);
             this.groupBox1.Location = new System.Drawing.Point(4, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(981, 69);
+            this.groupBox1.Size = new System.Drawing.Size(981, 64);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Operation";
             // 
+            // btn_step_3
+            // 
+            this.btn_step_3.Location = new System.Drawing.Point(182, 26);
+            this.btn_step_3.Name = "btn_step_3";
+            this.btn_step_3.Size = new System.Drawing.Size(75, 21);
+            this.btn_step_3.TabIndex = 2;
+            this.btn_step_3.Text = "Step 3";
+            this.btn_step_3.UseVisualStyleBackColor = true;
+            this.btn_step_3.Click += new System.EventHandler(this.btn_step_3_Click);
+            // 
+            // btn_step_2
+            // 
+            this.btn_step_2.Location = new System.Drawing.Point(101, 26);
+            this.btn_step_2.Name = "btn_step_2";
+            this.btn_step_2.Size = new System.Drawing.Size(75, 21);
+            this.btn_step_2.TabIndex = 1;
+            this.btn_step_2.Text = "Step 2";
+            this.btn_step_2.UseVisualStyleBackColor = true;
+            this.btn_step_2.Click += new System.EventHandler(this.btn_step_2_Click);
+            // 
             // btn_step_1
             // 
-            this.btn_step_1.Location = new System.Drawing.Point(20, 28);
+            this.btn_step_1.Location = new System.Drawing.Point(20, 26);
             this.btn_step_1.Name = "btn_step_1";
-            this.btn_step_1.Size = new System.Drawing.Size(75, 23);
+            this.btn_step_1.Size = new System.Drawing.Size(75, 21);
             this.btn_step_1.TabIndex = 0;
             this.btn_step_1.Text = "Step 1";
             this.btn_step_1.UseVisualStyleBackColor = true;
@@ -64,11 +86,14 @@
             // 
             // tabControl1
             // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.Text);
-            this.tabControl1.Location = new System.Drawing.Point(4, 77);
+            this.tabControl1.Location = new System.Drawing.Point(4, 71);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(985, 487);
+            this.tabControl1.Size = new System.Drawing.Size(985, 450);
             this.tabControl1.TabIndex = 1;
             // 
             // Text
@@ -77,7 +102,7 @@
             this.Text.Location = new System.Drawing.Point(4, 22);
             this.Text.Name = "Text";
             this.Text.Padding = new System.Windows.Forms.Padding(3);
-            this.Text.Size = new System.Drawing.Size(977, 461);
+            this.Text.Size = new System.Drawing.Size(977, 424);
             this.Text.TabIndex = 0;
             this.Text.Text = "Text";
             this.Text.UseVisualStyleBackColor = true;
@@ -91,36 +116,16 @@
             this.txt_result.Multiline = true;
             this.txt_result.Name = "txt_result";
             this.txt_result.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txt_result.Size = new System.Drawing.Size(971, 455);
+            this.txt_result.Size = new System.Drawing.Size(971, 418);
             this.txt_result.TabIndex = 0;
             this.txt_result.WordWrap = false;
             this.txt_result.TextChanged += new System.EventHandler(this.txt_result_TextChanged);
             // 
-            // btn_step_2
-            // 
-            this.btn_step_2.Location = new System.Drawing.Point(101, 28);
-            this.btn_step_2.Name = "btn_step_2";
-            this.btn_step_2.Size = new System.Drawing.Size(75, 23);
-            this.btn_step_2.TabIndex = 1;
-            this.btn_step_2.Text = "Step 2";
-            this.btn_step_2.UseVisualStyleBackColor = true;
-            this.btn_step_2.Click += new System.EventHandler(this.btn_step_2_Click);
-            // 
-            // btn_step_3
-            // 
-            this.btn_step_3.Location = new System.Drawing.Point(182, 28);
-            this.btn_step_3.Name = "btn_step_3";
-            this.btn_step_3.Size = new System.Drawing.Size(75, 23);
-            this.btn_step_3.TabIndex = 2;
-            this.btn_step_3.Text = "Step 3";
-            this.btn_step_3.UseVisualStyleBackColor = true;
-            this.btn_step_3.Click += new System.EventHandler(this.btn_step_3_Click);
-            // 
             // frm_match_500_team
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(992, 566);
+            this.ClientSize = new System.Drawing.Size(992, 522);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.groupBox1);
             this.Name = "frm_match_500_team";
--- web_helper/frm_match_500_team.cs
+++ web_helper/frm_match_500_team.cs
@@ -40,7 +40,6 @@ namespace web_helper
                     string href = node.Attributes["href"].Value.ToString();
                     if (href.ToLower().Contains("seasonid"))
                     {
-
                         nodes.Add(node);
                     }
                 }
@@ -61,93 +60,132 @@ namespace web_helper
 
         private void btn_step_2_Click(object sender, EventArgs e)
         {
-            //string path = root_path + "lg_url.txt";
-            //StringBuilder sb = new StringBuilder();
-            //FileStream stream = (FileStream)File.Open(path, FileMode.Open);
-            //StreamReader reader = new StreamReader(stream);
-            //string line = "";
-            //while (line != null)
-            //{
-            //    line = reader.ReadLine();
-            //    if (!string.IsNullOrEmpty(line))
-            //    {
-            //        sb.AppendLine("http://liansai.500.com/" + line);
-            //    }
-            //}
-            //reader.Close();
-            //stream.Close();
-            //this.txt_result.Text = sb.ToString();
+            string path = root_path + "lg_url.txt";
             StringBuilder sb = new StringBuilder();
-            WebClient client = new WebClient();
-            string html = System.Text.Encoding.GetEncoding("GBK").GetString(client.DownloadData(root_url + "step2.htm"));
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(html);
-
-            HtmlNodeCollection nodes_all = doc.DocumentNode.SelectNodes(@"//*");
-            List<HtmlNode> nodes = new List<HtmlNode>();
-
-            foreach (HtmlNode node in nodes_all)
+            FileStream stream = (FileStream)File.Open(path, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+            string line = "";
+            while (line != null)
             {
-                if (node.Attributes.Contains("href"))
+                line = reader.ReadLine();
+                if (!string.IsNullOrEmpty(line))
                 {
-                    string href = node.Attributes["href"].Value.ToString();
-                    if (href.ToLower().Contains("teamid"))// || href.Contains("teamid"))
+                    string url = "http://liansai.500.com/" + line;
+                    WebClient client = new WebClient();
+                    string html = System.Text.Encoding.GetEncoding("GBK").GetString(client.DownloadData(url));
+                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                    doc.LoadHtml(html);
+
+                    HtmlNodeCollection nodes_all = doc.DocumentNode.SelectNodes(@"//*");
+                    List<HtmlNode> nodes = new List<HtmlNode>();
+
+                    foreach (HtmlNode node in nodes_all)
                     {
+                        if (node.Attributes.Contains("href"))
+                        {
+                            string href = node.Attributes["href"].Value.ToString();
+                            if (href.ToLower().Contains("teamid"))// || href.Contains("teamid"))
+                            {
 
-                        nodes.Add(node);
+                                nodes.Add(node);
+                            }
+                        }
                     }
-                }
-            }
 
-            sb.AppendLine("----");
-            write_line("teams.txt", "----");
-            foreach (HtmlNode node in nodes)
-            {
-                string href = "http://liansai.500.com/" + node.Attributes["href"].Value.ToString();
-                if(!string.IsNullOrEmpty(href))
-                {
-                    sb.AppendLine(href);
-                    write_line("teams.txt",  href);
+                    sb.AppendLine("----");
+                    write_line("teams.txt", "----");
+                    foreach (HtmlNode node in nodes)
+                    {
+                        string href = "http://liansai.500.com/" + node.Attributes["href"].Value.ToString();
+                        if (!string.IsNullOrEmpty(href))
+                        {
+                            sb.AppendLine(href);
+                            write_line("teams.txt", href);
+                        }
+                    }
+                    this.txt_result.Text = sb.ToString();
+                    Application.DoEvents();
                 }
             }
-            this.txt_result.Text = sb.ToString();
+            reader.Close();
+            stream.Close();
+
+
+
 
         }
 
         private void btn_step_3_Click(object sender, EventArgs e)
         {
+            string path = root_path + "teams.txt";
             StringBuilder sb = new StringBuilder();
-            WebClient client = new WebClient();
-            string html = System.Text.Encoding.GetEncoding("GBK").GetString(client.DownloadData(root_url + "step3.htm"));
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(html);
-
-            string root = @"/html[1]/body[1]/div[3]/div[5]/div[1]/div[3]/ul[1]/li";
-
-            HtmlNodeCollection nodes_all = doc.DocumentNode.SelectNodes(root);
-
-            sb.AppendLine("----");
-            write_line("teams_detail.txt", "----");
-            foreach (HtmlNode node in nodes_all)
+            FileStream stream = (FileStream)File.Open(path, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+            string line = "";
+            bool last = false;
+            while (line != null)
             {
-                string xpath = node.XPath + "/a[1]";
-                HtmlNode final = doc.DocumentNode.SelectNodes(xpath)[0];
-                string text = final.InnerText;
-                text = text.Replace('(', '●').Replace(')', ' ');
+                line = reader.ReadLine();
+                if (!string.IsNullOrEmpty(line) && line.Substring(0,2)!="--" && last==true)
+                {
 
-                string[] items = text.Split(new string[] { "●" }, StringSplitOptions.RemoveEmptyEntries);
-                sb.AppendLine(items[0].PR(20) + items[1].PR(20));
-                write_line("teams_detail.txt", items[0].PR(20) + items[1].PR(20));
+                    try
+                    { 
+                        WebClient client = new WebClient();
+                        string html = System.Text.Encoding.GetEncoding("GBK").GetString(client.DownloadData(line));
+                        HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                        doc.LoadHtml(html);
+
+                        string root = @"/html[1]/body[1]/div[3]/div[5]/div[1]/div[3]/ul[1]/li";
+
+                        HtmlNodeCollection nodes_all = doc.DocumentNode.SelectNodes(root);
+
+                        sb.AppendLine("----");
+                        write_line("teams_detail.txt", "----"+line);
+                        foreach (HtmlNode node in nodes_all)
+                        {
+                            string xpath = node.XPath + "/a[1]";
+                            HtmlNode final = doc.DocumentNode.SelectNodes(xpath)[0];
+                            string text = final.InnerText;
+                            text = text.Replace('(', '●').Replace(')', ' ');
+
+                            string[] items = text.Split(new string[] { "●" }, StringSplitOptions.RemoveEmptyEntries);
+                            if (items.Length > 1)
+                            {
+                                sb.AppendLine(items[0].PR(20) + items[1].PR(20));
+                                write_line("teams_detail.txt", items[0].PR(20) + items[1].PR(20));
+                            }
+                        }
+                        this.txt_result.Text = sb.ToString();
+                        Application.DoEvents();
+                    }
+                    catch(Exception error)
+                    {
+                        sb.AppendLine(error.Message);
+                        Application.DoEvents();
+                    }
+
+                   
+                }
+                if (!string.IsNullOrEmpty(line) && line.Substring(0, 2) == "--")
+                {
+                    last = true;
+                }
+                else
+                {
+                    last = false;
+                }
             }
-            this.txt_result.Text = sb.ToString();
+            //StringBuilder sb = new StringBuilder();
+           
         }
         public void write_line(string file_name, string txt)
         {
             FileStream stream = (FileStream)File.Open(root_path + file_name, FileMode.Append);
             StreamWriter writer = new StreamWriter(stream);
-            
+
             writer.WriteLine(txt);
-            writer.Close(); 
+            writer.Close();
             stream.Close();
         }
     }
