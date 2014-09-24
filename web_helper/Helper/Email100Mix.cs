796588fd2e27235516d51981c112faa7731e811a
Long Yang <598251812@qq.com>
2014-09-24 16:46:09 +0800

    add Match100Max class
    add Match100Max class
    modify compute method.xls

---
+++ web_helper/Helper/Email100Mix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+
+class Email100Mix
+{
+    static bool is_open_mongo = false;
+
+    public static  BsonDocument get_document()
+    {
+        BsonDocument doc = new BsonDocument();
+        doc.Add("start_time", "");
+        doc.Add("host", "");
+        doc.Add("client", ""); 
+
+        return doc;
+
+    }
+    public void compute()
+    {
+        BsonDocument doc = new BsonDocument();
+        if (is_open_mongo) MongoHelper.insert_bson("email", doc); 
+    }
+}
+
