c73b87a307672567f8183a4d2dc95f9cb78be7b8
yanglong <598251812@qq.com>
2014-10-13 22:12:20 +0800

    modify some bug
    modify some bug

--- web_helper/data/sql/pinnaclesports_marathonbet_matching.sql
+++ web_helper/data/sql/pinnaclesports_marathonbet_matching.sql
@@ -5,10 +5,11 @@ select a.start_time,a.host,a.client,a.website,a.timespan,a.odd_win,a.odd_draw,a.
        b.website,b.timespan,b.odd_win,b.odd_draw,b.odd_lose
 from europe_100 a
 left join europe_100 b on a.start_time=b.start_time and a.host=b.host and a.client=b.client
-where  a.id in  (select max(id) from europe_100 where  start_time>@START_DATE group by website,start_time,host,client)
-and    b.id in  (select max(id) from europe_100 where  start_time>@START_DATE group by website,start_time,host,client)
+where  a.id in  (select max(id) from europe_100 where  start_time>'2014-10-13 00:00:00' group by website,start_time,host,client)
+and    b.id in  (select max(id) from europe_100 where  start_time>'2014-10-13 00:00:00' group by website,start_time,host,client)
 and    a.website ='pinnaclesports'
 and    b.website='marathonbet' 
+order by a.start_time
 
 
 ----the count that two sites's distinct log 
@@ -17,15 +18,14 @@ select count(*) from
 (
 	select distinct website,start_time,host,client,odd_win,odd_draw,odd_lose
 	from  europe_100_log 
-	where f_start_time>@START_DATE and website='pinnaclesports'
+	where f_start_time>'2014-10-13 00:00:00'  and timespan >'2014-10-13 00:00:00' and website='pinnaclesports'
 ) a
 union all
 select count(*) from
 (
 	select distinct website,start_time,host,client,odd_win,odd_draw,odd_lose
 	from  europe_100_log 
-	where f_start_time>@START_DATE and website='marathonbet'
+	where f_start_time>'2014-10-13 00:00:00' and timespan >'2014-10-13 00:00:00' and website='marathonbet'
 ) b
-
 
 
