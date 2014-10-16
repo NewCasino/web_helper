800c327553d3ee706d5fc6a5b77b8e1d69976b58
yanglong <598251812@qq.com>
2014-10-16 21:13:20 +0800

    add some data
    add some data

--- web_helper/frm_match_100_analyse_with_present.Designer.cs
+++ web_helper/frm_match_100_analyse_with_present.Designer.cs
@@ -64,14 +64,14 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.groupBox1.Controls.Add(this.groupBox5);
             this.groupBox1.Controls.Add(this.txt_condition);
             this.groupBox1.Controls.Add(this.btn_load);
             this.groupBox1.Location = new System.Drawing.Point(6, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(264, 558);
+            this.groupBox1.Size = new System.Drawing.Size(264, 515);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Operation";
@@ -87,9 +87,9 @@
             this.groupBox5.Controls.Add(this.cb_two_persent_desc);
             this.groupBox5.Controls.Add(this.cb_two_persent_asc);
             this.groupBox5.Controls.Add(this.btn_compute);
-            this.groupBox5.Location = new System.Drawing.Point(6, 59);
+            this.groupBox5.Location = new System.Drawing.Point(6, 54);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(252, 176);
+            this.groupBox5.Size = new System.Drawing.Size(252, 162);
             this.groupBox5.TabIndex = 7;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Present";
@@ -97,43 +97,43 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 55);
+            this.label2.Location = new System.Drawing.Point(8, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.Size = new System.Drawing.Size(77, 12);
             this.label2.TabIndex = 8;
             this.label2.Text = "Present Add:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 25);
+            this.label1.Location = new System.Drawing.Point(8, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.Size = new System.Drawing.Size(71, 12);
             this.label1.TabIndex = 8;
             this.label1.Text = "Present ID:";
             // 
             // txt_add
             // 
-            this.txt_add.Location = new System.Drawing.Point(81, 52);
+            this.txt_add.Location = new System.Drawing.Point(85, 48);
             this.txt_add.Name = "txt_add";
-            this.txt_add.Size = new System.Drawing.Size(149, 20);
+            this.txt_add.Size = new System.Drawing.Size(145, 21);
             this.txt_add.TabIndex = 17;
             this.txt_add.Text = "0.2";
             // 
             // txt_id
             // 
-            this.txt_id.Location = new System.Drawing.Point(81, 19);
+            this.txt_id.Location = new System.Drawing.Point(85, 18);
             this.txt_id.Name = "txt_id";
-            this.txt_id.Size = new System.Drawing.Size(149, 20);
+            this.txt_id.Size = new System.Drawing.Size(145, 21);
             this.txt_id.TabIndex = 16;
             this.txt_id.Text = "3812";
             // 
             // cb_two_start_time_desc
             // 
             this.cb_two_start_time_desc.AutoSize = true;
-            this.cb_two_start_time_desc.Location = new System.Drawing.Point(127, 110);
+            this.cb_two_start_time_desc.Location = new System.Drawing.Point(127, 102);
             this.cb_two_start_time_desc.Name = "cb_two_start_time_desc";
-            this.cb_two_start_time_desc.Size = new System.Drawing.Size(87, 17);
+            this.cb_two_start_time_desc.Size = new System.Drawing.Size(90, 16);
             this.cb_two_start_time_desc.TabIndex = 15;
             this.cb_two_start_time_desc.Text = "S-time DESC";
             this.cb_two_start_time_desc.UseVisualStyleBackColor = true;
@@ -141,9 +141,9 @@
             // cb_two_start_time_asc
             // 
             this.cb_two_start_time_asc.AutoSize = true;
-            this.cb_two_start_time_asc.Location = new System.Drawing.Point(15, 110);
+            this.cb_two_start_time_asc.Location = new System.Drawing.Point(15, 102);
             this.cb_two_start_time_asc.Name = "cb_two_start_time_asc";
-            this.cb_two_start_time_asc.Size = new System.Drawing.Size(83, 17);
+            this.cb_two_start_time_asc.Size = new System.Drawing.Size(84, 16);
             this.cb_two_start_time_asc.TabIndex = 14;
             this.cb_two_start_time_asc.Text = "S-Time ASC";
             this.cb_two_start_time_asc.UseVisualStyleBackColor = true;
@@ -151,9 +151,9 @@
             // cb_two_persent_desc
             // 
             this.cb_two_persent_desc.AutoSize = true;
-            this.cb_two_persent_desc.Location = new System.Drawing.Point(127, 87);
+            this.cb_two_persent_desc.Location = new System.Drawing.Point(127, 80);
             this.cb_two_persent_desc.Name = "cb_two_persent_desc";
-            this.cb_two_persent_desc.Size = new System.Drawing.Size(94, 17);
+            this.cb_two_persent_desc.Size = new System.Drawing.Size(96, 16);
             this.cb_two_persent_desc.TabIndex = 12;
             this.cb_two_persent_desc.Text = "Persent DESC";
             this.cb_two_persent_desc.UseVisualStyleBackColor = true;
@@ -163,18 +163,18 @@
             this.cb_two_persent_asc.AutoSize = true;
             this.cb_two_persent_asc.Checked = true;
             this.cb_two_persent_asc.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cb_two_persent_asc.Location = new System.Drawing.Point(15, 87);
+            this.cb_two_persent_asc.Location = new System.Drawing.Point(15, 80);
             this.cb_two_persent_asc.Name = "cb_two_persent_asc";
-            this.cb_two_persent_asc.Size = new System.Drawing.Size(86, 17);
+            this.cb_two_persent_asc.Size = new System.Drawing.Size(90, 16);
             this.cb_two_persent_asc.TabIndex = 11;
             this.cb_two_persent_asc.Text = "Persent ASC";
             this.cb_two_persent_asc.UseVisualStyleBackColor = true;
             // 
             // btn_compute
             // 
-            this.btn_compute.Location = new System.Drawing.Point(72, 136);
+            this.btn_compute.Location = new System.Drawing.Point(72, 126);
             this.btn_compute.Name = "btn_compute";
-            this.btn_compute.Size = new System.Drawing.Size(75, 23);
+            this.btn_compute.Size = new System.Drawing.Size(75, 21);
             this.btn_compute.TabIndex = 5;
             this.btn_compute.Text = "Compute";
             this.btn_compute.UseVisualStyleBackColor = true;
@@ -182,16 +182,16 @@
             // 
             // txt_condition
             // 
-            this.txt_condition.Location = new System.Drawing.Point(6, 22);
+            this.txt_condition.Location = new System.Drawing.Point(6, 20);
             this.txt_condition.Name = "txt_condition";
-            this.txt_condition.Size = new System.Drawing.Size(171, 20);
+            this.txt_condition.Size = new System.Drawing.Size(171, 21);
             this.txt_condition.TabIndex = 3;
             // 
             // btn_load
             // 
-            this.btn_load.Location = new System.Drawing.Point(183, 21);
+            this.btn_load.Location = new System.Drawing.Point(183, 19);
             this.btn_load.Name = "btn_load";
-            this.btn_load.Size = new System.Drawing.Size(75, 23);
+            this.btn_load.Size = new System.Drawing.Size(75, 21);
             this.btn_load.TabIndex = 2;
             this.btn_load.Text = "Load";
             this.btn_load.UseVisualStyleBackColor = true;
@@ -199,16 +199,16 @@
             // 
             // tabControl1
             // 
-            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.Condition);
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Location = new System.Drawing.Point(276, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(916, 558);
+            this.tabControl1.Size = new System.Drawing.Size(916, 515);
             this.tabControl1.TabIndex = 1;
             // 
             // Condition
@@ -216,31 +216,31 @@
             this.Condition.Controls.Add(this.groupBox2);
             this.Condition.Location = new System.Drawing.Point(4, 22);
             this.Condition.Name = "Condition";
-            this.Condition.Size = new System.Drawing.Size(908, 532);
+            this.Condition.Size = new System.Drawing.Size(908, 489);
             this.Condition.TabIndex = 2;
             this.Condition.Text = "Condition";
             this.Condition.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.Controls.Add(this.btn_match_reverse);
             this.groupBox2.Controls.Add(this.btn_match_all);
             this.groupBox2.Controls.Add(this.dgv_match);
             this.groupBox2.Location = new System.Drawing.Point(3, 3);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(897, 527);
+            this.groupBox2.Size = new System.Drawing.Size(897, 486);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Condition";
             // 
             // btn_match_reverse
             // 
-            this.btn_match_reverse.Location = new System.Drawing.Point(87, 22);
+            this.btn_match_reverse.Location = new System.Drawing.Point(87, 20);
             this.btn_match_reverse.Name = "btn_match_reverse";
-            this.btn_match_reverse.Size = new System.Drawing.Size(75, 25);
+            this.btn_match_reverse.Size = new System.Drawing.Size(75, 23);
             this.btn_match_reverse.TabIndex = 2;
             this.btn_match_reverse.Text = "REVERSE";
             this.btn_match_reverse.UseVisualStyleBackColor = true;
@@ -248,9 +248,9 @@
             // 
             // btn_match_all
             // 
-            this.btn_match_all.Location = new System.Drawing.Point(6, 22);
+            this.btn_match_all.Location = new System.Drawing.Point(6, 20);
             this.btn_match_all.Name = "btn_match_all";
-            this.btn_match_all.Size = new System.Drawing.Size(75, 25);
+            this.btn_match_all.Size = new System.Drawing.Size(75, 23);
             this.btn_match_all.TabIndex = 1;
             this.btn_match_all.Text = "ALL";
             this.btn_match_all.UseVisualStyleBackColor = true;
@@ -258,13 +258,13 @@
             // 
             // dgv_match
             // 
-            this.dgv_match.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv_match.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv_match.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_match.Location = new System.Drawing.Point(6, 59);
+            this.dgv_match.Location = new System.Drawing.Point(6, 54);
             this.dgv_match.Name = "dgv_match";
-            this.dgv_match.Size = new System.Drawing.Size(885, 460);
+            this.dgv_match.Size = new System.Drawing.Size(885, 425);
             this.dgv_match.TabIndex = 0;
             this.dgv_match.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dgv_match_DataBindingComplete);
             // 
@@ -274,7 +274,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(908, 532);
+            this.tabPage1.Size = new System.Drawing.Size(908, 489);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "All Data";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -285,7 +285,7 @@
             this.dgv_all.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgv_all.Location = new System.Drawing.Point(3, 3);
             this.dgv_all.Name = "dgv_all";
-            this.dgv_all.Size = new System.Drawing.Size(902, 526);
+            this.dgv_all.Size = new System.Drawing.Size(902, 483);
             this.dgv_all.TabIndex = 0;
             // 
             // tabPage2
@@ -294,7 +294,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(908, 532);
+            this.tabPage2.Size = new System.Drawing.Size(908, 489);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Result Text";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -309,16 +309,16 @@
             this.txt_result.Multiline = true;
             this.txt_result.Name = "txt_result";
             this.txt_result.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txt_result.Size = new System.Drawing.Size(902, 526);
+            this.txt_result.Size = new System.Drawing.Size(902, 483);
             this.txt_result.TabIndex = 0;
             this.txt_result.WordWrap = false;
             this.txt_result.TextChanged += new System.EventHandler(this.txt_result_TextChanged);
             // 
             // frm_match_100_analyse_with_present
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1192, 566);
+            this.ClientSize = new System.Drawing.Size(1192, 522);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.groupBox1);
             this.Name = "frm_match_100_analyse_with_present";
