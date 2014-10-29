3acfda3feb804c2d3fa9a2b1652286a41ce3b8d7
yanglong <598251812@qq.com>
2014-10-29 21:28:33 +0800

    add some data
    add some data

--- web_helper/Helper/Match100Method.cs
+++ web_helper/Helper/Match100Method.cs
@@ -1330,6 +1330,10 @@ class Match100Method
                             lose = Match100Helper.convert_english_odd(node_table.SELECT_NODE("/tr[1]/td[4]").InnerText);
                             sb.AppendLine(league.PR(50) + start_time.PR(20) + host.PR(30) + client.PR(30) + win.PR(10) + draw.PR(10) + lose.PR(10));
                             Match100Helper.insert_data("marathonbet", league, start_time, host, client, win, draw, lose, "0", "0");
+                            Match100Helper.insert_data("marathonbet", league, start_time, host, client, win, draw, lose, "1", "0");
+                            Match100Helper.insert_data("marathonbet", league, start_time, host, client, win, draw, lose, "-1", "0");
+                            Match100Helper.insert_data("marathonbet", league, start_time, host, client, win, draw, lose, "2", "0");
+                            Match100Helper.insert_data("marathonbet", league, start_time, host, client, win, draw, lose, "-2", "0");
                         }
 
                     }
