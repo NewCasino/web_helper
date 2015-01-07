f9dcd52b9b9f5b233d6e21efd83ae1e42a0aaa58
Long Yang <598251812@qq.com>
2015-01-07 11:57:39 +0800

    add  web_display
    add web_display

---
+++ web_display/App_Code/CurrencyHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text; 
+using MongoDB.Bson; 
+using System.Collections;
+
+
+public class CurrencyHelper
+{
+    public static void insert_currency(string website,string c_from,string c_to,string c_rate)
+    {
+        string timespan = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ffffff");
+        string sql = "";
+
+        sql = "delete from currency where website='{0}' and c_from='{1}'and c_to='{2}'";
+        sql = string.Format(sql, website, c_from, c_to);
+        SQLServerHelper.exe_sql(sql);
+
+        sql = "insert into currency (timespan,website,c_from,c_to,c_rate) values('{0}','{1}','{2}','{3}','{4}')";
+        sql = string.Format(sql, timespan, website, c_from, c_to, c_rate);
+        SQLServerHelper.exe_sql(sql);
+
+        sql = "insert into currency_log (timespan,website,c_from,c_to,c_rate) values('{0}','{1}','{2}','{3}','{4}')";
+        sql = string.Format(sql, timespan, website, c_from, c_to, c_rate);
+        SQLServerHelper.exe_sql(sql); 
+    }
+    public static string  get_simple_name(string cn_name)
+    {
+        switch (cn_name)
+        {
+
+            case "美元":
+                return "usd";
+                break;
+            case "欧元":
+                return "eur";
+                break;
+            case "港币":
+                return "hkd";
+                break;
+            case "新西兰元":
+                return "nzd";
+                break;
+            case "澳大利亚元":
+                return "aud";
+                break;
+            case "加拿大元":
+                return "cad";
+                break;
+            case "英镑":
+                return "gbp";
+                break;
+            case "日元":
+                return "jpy";
+                break;
+            case "新加坡元":
+                return "sgd";
+                break;
+            case "瑞士法郎":
+                return "chf";
+                break;
+            default:
+                return "no";
+                break;
+
+        }
+    }
+    public static double get_rate(string from, string to)
+    {
+        string sql = "";
+        sql = "select c_rate from currency where website='cmbchina' and c_from='{0}' and c_to='{1}'";
+        sql = string.Format(sql,from, to);
+        double rate = Convert.ToDouble(SQLServerHelper.get_table(sql).Rows[0][0].ToString());
+        return rate;
+    }
+}
+
---
+++ web_display/App_Code/SQLServerHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+public class SQLServerHelper
+{
+ 
+     public static string str_con = @"Data Source=yanglong\SQLSERVER2012;Initial Catalog=btce;Integrated Security=True";
+     //public static string str_con = @"Data Source=.;Initial Catalog=btce;Integrated Security=True";
+    public static DataTable get_table(string sql)
+    {
+        if (Environment.UserName == "CY120467") SQLServerHelper.str_con = "Data Source=.;Initial Catalog=btce;Integrated Security=True";
+        SqlConnection con = new SqlConnection(str_con);
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = sql;
+        cmd.CommandType = CommandType.Text; 
+        SqlDataAdapter dap = new SqlDataAdapter();
+        dap.SelectCommand = cmd;
+        DataSet ds = new DataSet();
+        dap.Fill(ds);
+        return ds.Tables[0];
+    }
+    public static void exe_sql(string sql)
+    {
+        if (Environment.UserName == "CY120467") SQLServerHelper.str_con = "Data Source=.;Initial Catalog=btce;Integrated Security=True";
+        SqlConnection con = new SqlConnection(str_con);
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = sql;
+        cmd.CommandType = CommandType.Text;
+
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+    } 
+    public static string format_sql_str(string sql)
+    {
+        sql = sql.Replace("'", "''");
+        return sql;         
+    }
+}
+
