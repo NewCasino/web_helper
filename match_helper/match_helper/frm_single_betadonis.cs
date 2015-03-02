1f891b375852be7a55d8895b3484e7cdebeb6782
Long Yang <598251812@qq.com>
2015-03-02 16:06:27 +0800

    modify

--- match_helper/match_helper/frm_single_betadonis.cs
+++ match_helper/match_helper/frm_single_betadonis.cs
@@ -38,7 +38,7 @@ namespace web_helper
 
         private void btn_get_data_Click(object sender, EventArgs e)
         {
-            this.txt_result.Text = get_data(this.txt_result.Text);
+            this.txt_result.Text = get_data2(this.txt_result.Text);
         }
         public string get_html()
         {
@@ -49,16 +49,16 @@ namespace web_helper
         {
 
             StringBuilder sb = new StringBuilder();
- 
+
             HtmlAgilityPack.HtmlDocument doc_html = new HtmlAgilityPack.HtmlDocument();
             doc_html.LoadHtml(html);
 
 
-            HtmlNodeCollection nodes_all = doc_html.DocumentNode.SelectNodes(@"//*"); 
+            HtmlNodeCollection nodes_all = doc_html.DocumentNode.SelectNodes(@"//*");
             //string str_json = doc_html.DocumentNode.SELECT_NODE("/div[1]/div[1]/script[1]").InnerText.Replace("var centralAreaLiveSectionsJsonArray =", "");
-            string str_json = doc_html.DocumentNode.SELECT_NODE("/div[1]/div[2]/script[1]").InnerText.Replace("var centralAreaPreLiveSectionsJsonArray =", ""); 
+            string str_json = doc_html.DocumentNode.SELECT_NODE("/div[1]/div[2]/script[1]").InnerText.Replace("var centralAreaPreLiveSectionsJsonArray =", "");
             BsonArray list_root = MongoHelper.get_array_from_str(str_json);
-            foreach (BsonDocument  root  in list_root)
+            foreach (BsonDocument root in list_root)
             {
                 BsonArray matchs = root["matchesJsonArray"].AsBsonArray;
                 foreach (BsonDocument match in matchs)
@@ -67,24 +67,62 @@ namespace web_helper
                     {
                         string date = match["matchInfo"]["sdfDMYTime"].ToString();
                         string time = match["matchInfo"]["sdfHourTime"].ToString();
-                        string start_time = DateTime.Now.Year.ToString() + "-" + date.Substring(3, 2) + "-" + date.Substring(0, 2) + " " + time ;
+                        string start_time = DateTime.Now.Year.ToString() + "-" + date.Substring(3, 2) + "-" + date.Substring(0, 2) + " " + time;
                         string home = match["matchInfo"]["homeName"].ToString();
                         string away = match["matchInfo"]["awayName"].ToString();
                         string odd_home = match["marketWrapper"]["oddsJsonArray"][0]["ov"].ToString();
                         string odd_draw = match["marketWrapper"]["oddsJsonArray"][1]["ov"].ToString();
                         string odd_away = match["marketWrapper"]["oddsJsonArray"][2]["ov"].ToString();
-                        sb.Append(start_time.PR(20) + home.PR(20) + away.PR(20) +odd_home.PR(10)+odd_draw.PR(10)+odd_away.PR(10)+ M.N);
+                        sb.Append(start_time.PR(20) + home.PR(20) + away.PR(20) + odd_home.PR(10) + odd_draw.PR(10) + odd_away.PR(10) + M.N);
                         Match100Helper.insert_data("betadonis", "", start_time, home, away, odd_home, odd_draw, odd_away, "0", "0");
                     }
                 }
-            }
-
-
-            return sb.ToString();
-            
-
+            } 
+            return sb.ToString(); 
         }
+        public string get_data2(string html)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            html = html.Replace("<thead=\"\"", "");
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+
+            HtmlNodeCollection nodes_all = doc.DocumentNode.SelectNodes(@"//*");
+            List<HtmlNode> nodes = new List<HtmlNode>();
 
 
+            foreach (HtmlNode node in nodes_all)
+            {
+                if (node.CLASS() == "MatchContainer")
+                { 
+                    if (!node.InnerHtml.Contains( "LiveMatchCompetitors"))
+                    {
+                        string date = node.SELECT_NODE("/div[1]/span[1]/strong[1]").ChildNodes[0].InnerText.E_TRIM();
+                        string time = node.SELECT_NODE("/div[1]/span[1]/em[1]").ChildNodes[0].InnerText.E_TRIM(); 
+                        string start_time = ""; 
+                        if (date.E_TRIM().ToLower() != "live")
+                        {
+                            start_time = date + M.D + time;
+                        }
+                        else
+                        {
+                            start_time = DateTime.Now.ToString("mm/dd") + M.D + time;
+                        }
+
+                        string host = node.SELECT_NODE("/div[2]/h3[1]/a[1]/span[1]").ChildNodes[0].InnerText;
+                        string client = node.SELECT_NODE("/div[2]/h3[1]/a[1]/span[3]").ChildNodes[0].InnerText; 
+
+                        string odd_host = node.SELECT_NODE("div[4]/ol[1]/li[1]/a[1]/span[1]").InnerText;
+                        string odd_draw = node.SELECT_NODE("div[4]/ol[1]/li[2]/a[1]/span[1]").InnerText;
+                        string odd_away = node.SELECT_NODE("div[4]/ol[1]/li[3]/a[1]/span[1]").InnerText;
+                        BsonDocument doc_odd = Match100Helper.get_odd_doc_from_europe(odd_host, odd_draw, odd_away);
+                        sb.Append(start_time.PR(20) + host.PR(30) + client.PR(30) + odd_host.PR(10) + odd_draw.PR(10) + odd_away.PR(10) + doc_odd["persent_return"].ToString() + M.N);
+                        Match100Helper.insert_data("betadonis", "", start_time, host, client, odd_host, odd_draw, odd_away, "0", "0");
+                    }
+                }
+            }
+            return sb.ToString();
+        } 
     }
 }
