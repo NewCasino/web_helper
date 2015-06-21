912d8ab51324ba222afc5495b4780e1afe1d7388
yanglong <598251812@qq.com>
2015-06-22 03:32:59 +0800

    night

--- web_display/media/js_new/index.js
+++ web_display/media/js_new/index.js
@@ -5,7 +5,7 @@ var Index = function () {
             $('#style_color').attr("href", "assets/css/themes/light.css");
             $.cookie('style_color', 'light');  
             //$('body').addClass('page-sidebar-closed').removeClass('page-sidebar-hover-on');      
-            $("#container_data").load("empty.html"); 
+            $("#container_data").load("chart_stock.html");
             $("a[menu]").click(function(){
               alert("the page ["+ $(this).attr("menu")+"] is developing......"); 
             
